675102c20ffca3e435dfc7175be9f9bee0b835a6
Mike Overy <movery@mac.com>
2019-12-23 18:05:50 +0000

    Add support for always on display

--- app/index.js
+++ app/index.js
@@ -8,6 +8,7 @@ import * as battery from "./battery";
 import * as heartMonitor from "./hrm";
 import * as util from "../common/utils";
 import { locale } from "user-settings";
+import { me } from "appbit";
 
 // Set up all necessary variables
 let clockTextH   = document.getElementById("clockTextH");
@@ -17,17 +18,86 @@ let amCircle   = document.getElementById("amCircle");
 let pmCircle   = document.getElementById("pmCircle");
 let stepProg1   = document.getElementById("stepProg1");
 let stepProg2   = document.getElementById("stepProg2");
+
 clock.granularity = "seconds";
 let date         = document.getElementById("date");
+let Batt         = document.getElementById("Batt");
+let Data         = document.getElementById("Data");
+let AOD          = document.getElementById("AOD");
 
 let dataTypes     = [ "steps", "distance", "calories",
                       "elevationGain", "activeMinutes" ];
 let dataProgress  = [];
 let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-let statItem = document.getElementById("clickbg");
-let curstat = "";
-let statidx = 0;
+let whiteCounter = -1;
+
+var bgImage = document.getElementById("bgImage");
+bgImage.href = "bgBlack.jpg";
+
+clockTextH.onclick = (e) => {
+  if (display.aodActive == false)
+  {
+    console.log("hour clicked");
+    flipDisplay();
+  }
+}
+
+if ( display.aodAvailable) {
+  console.log(me.permissions.granted("access_aod"));
+  display.aodAllowed = true;
+}
+
+clockTextM.onclick = (e) => {
+  if (display.aodActive == false)
+  {
+    console.log("minute clicked");
+    console.log("aodAvailable: " + display.aodAvailable);
+    console.log("aodActive: " + display.aodActive);
+    console.log("aodAllowed: " + display.aodAllowed);
+    console.log("aodEnabled: " + display.aodEnabled);   
+    console.log(Batt.style.display);
+  }
+}
+
+display.addEventListener("change", () => {
+  console.log("aodActive: " + display.aodActive);
+   if (display.aodActive) {
+       console.log ("Always on Enabled")
+       bgImage.href = "bgBlack.jpg";
+       clockTextH.style.fill = "white";
+       clockTextM.style.fill = "white";
+       whiteCounter = -1;
+       Batt.style.display = "none";
+       Data.style.display = "none";
+       AOD.style.display = "inline";
+       clock.granularity = 'minutes';
+       date.style.fill = "white";
+       display.brightnessOverride = "dim";
+   } else {
+       console.log ("Always on Disabled")
+       Batt.style.display = "inline";
+       Data.style.display = "inline";
+       clock.granularity = "seconds";
+       display.brightnessOverride = "normal";
+       date.style.fill = "yellow";
+       AOD.style.display = "none";
+   }
+});
+
+function flipDisplay() {
+  if (bgImage.href === "bgBlack.jpg") {
+      bgImage.href = "bgWhite.jpg";
+      clockTextH.style.fill = "black";
+      clockTextM.style.fill = "black";
+      whiteCounter = 10;
+  } else {
+      bgImage.href = "bgBlack.jpg";
+      clockTextH.style.fill = "white";
+      clockTextM.style.fill = "white";
+      whiteCounter = -1;
+  }  
+}
 
 let getCurrentDataProgress = function(dataType) {
   let dataContainer = document.getElementById(dataType);
@@ -49,12 +119,21 @@ for(var i=0; i < dataTypes.length; i++) {
 // Refresh data, all other logic is in separate files
 function refreshData(type) {
   let currentType = type.dataType;
+  let dataValid = 1;
   
   let currentDataProg = (userActivity.today.adjusted[currentType] || 0);
   let currentDataGoal = userActivity.goals[currentType];
+  if (currentDataGoal == undefined)
+  {
+    currentDataGoal = 1;
+    dataValid = 0;
+  }
   
   let currentDataArc = (currentDataProg / currentDataGoal) * 360;
   //currentDataArc = 90;
+  //console.log (currentType);
+  //console.log(currentDataProg);
+  //console.log(currentDataGoal);
   if(currentType!="steps") {
     if (currentDataArc >= 360) {
       currentDataArc = 360;
@@ -87,7 +166,7 @@ function refreshData(type) {
   }
   
   if(currentType==="distance") {
-    currentDataProg = (currentDataProg * 0.000621371192).toFixed(2);
+    currentDataProg = (currentDataProg * 0.000621371192).toFixed(1);
   }
   if(currentType==="calories") {
     if (currentDataProg >= 1000) {
@@ -107,13 +186,18 @@ function refreshData(type) {
         type.dataCount.style.fill = "lightgreen";
       }
       else {
-        let currentDataProg1 = currentDataProg - userActivity.goals[currentType];
+        let currentDataProg1 = currentDataProg;
+        if (userActivity.goals[currentType] != undefined)
+        {
+          currentDataProg1 = currentDataProg1 - userActivity.goals[currentType];
+        }
+        
         //currentDataProg = `+${currentDataProg}`;
         stepProg1.width = 276;
         stepProg2.width = (currentDataProg1 / currentDataGoal) * 276;
         stepProg1.style.fill = "lightgreen";
         stepProg2.style.fill = "lightblue";
-        type.dataCount.style.fill = "lightgreen";          
+        type.dataCount.style.fill = "lightgreen";   
       }
     }
     else {
@@ -124,37 +208,16 @@ function refreshData(type) {
       type.dataCount.style.fill = "lightblue";
     }
   }
-  type.dataCount.text = currentDataProg;
-  
-}
-
-statItem.onclick = evt => {
-  console.log ("click");
-  statidx++;
-  if (statidx == dataTypes.length) {
-    statidx = 0;
-  }
-  let currentData = dataTypes[statidx];
-  //console.log (currentData);
-  let dataContainer = document.getElementById("distance");
-  let myicon = dataContainer.getElementById("dataIcon");
-  if (currentData - "distance")
+  if (dataValid == 1)
   {
-    myicon.href = "icons/distOpen.png";
+     //console.log(currentType + ": " + currentDataProg)
+     type.dataCount.text = currentDataProg;
   }
-  if (currentData - "calories")
+  else
   {
-    myicon.href = "icons/calsOpen.png";
+     type.dataCount.text = "";
   }
-  if (currentData - "elevationGain")
-  {
-    myicon.href = "icons/floorsOpen.png";
-  }
-  if (currentData - "activeMinutes")
-  {
-    myicon.href = "icons/amOpen.png";
-  }
-
+  //console.log (currentType);
 }
 
 function refreshAllData() {
@@ -168,6 +231,16 @@ clock.ontick = evt => {
   let hours = today.getHours();
   let mins  = util.zeroPad(today.getMinutes());
   let secs  = util.zeroPad(today.getSeconds()); 
+  if (whiteCounter >= 0) {
+      whiteCounter = whiteCounter -1;
+      //console.log (whiteCounter);
+  }
+  if (whiteCounter == 0)
+  {
+      bgImage.href = "bgBlack.jpg";
+      clockTextH.style.fill = "white";
+      clockTextM.style.fill = "white";
+  }
   
   if (hours < 12) {
     amCircle.style.fill = 'yellow';
@@ -182,14 +255,14 @@ clock.ontick = evt => {
   clockTextH.text = `${hours}`;
   clockTextM.text = `${mins}`;
   clockTextS.text = `${secs}`;
-
   let day      = today.getDate();
   let dow      = days[today.getDay()];
-  date.text = dow + '   ' + day;
-  
-  battery.setLevel();
-  
-  refreshAllData();
+  date.text = dow + '   ' + day;  
+
+  if (display.aodActive == false) {
+    refreshAllData();
+  }
+  battery.setLevel();  
 }
 
 heartMonitor.initialize();
